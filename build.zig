a3ea8a874955635d4bf2db1fc37b6e030f8ded7c
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2023-10-07 15:17:03 -0700

    use latest zig APIs

--- build.zig
+++ build.zig
@@ -5,26 +5,14 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     const lib = b.addStaticLibrary(.{
-        .name = "xcode-frameworks",
+        .name = "macos-sdk",
         .root_source_file = .{ .path = "stub.c" },
         .target = target,
         .optimize = optimize,
     });
     lib.linkLibC();
-    lib.installHeadersDirectory("include", ".");
+    lib.addSystemFrameworkPath(.{ .path = "Frameworks" });
+    lib.addSystemIncludePath(.{ .path = "include" });
+    lib.addLibraryPath(.{ .path = "lib" });
     b.installArtifact(lib);
-}
-
-pub fn addPaths(step: *std.build.CompileStep) void {
-    step.addFrameworkPath(sdkPath("/Frameworks"));
-    step.addSystemIncludePath(sdkPath("/include"));
-    step.addLibraryPath(sdkPath("/lib"));
-}
-
-fn sdkPath(comptime suffix: []const u8) []const u8 {
-    if (suffix[0] != '/') @compileError("suffix must be an absolute path");
-    return comptime blk: {
-        const root_dir = std.fs.path.dirname(@src().file) orelse ".";
-        break :blk root_dir ++ suffix;
-    };
 }
