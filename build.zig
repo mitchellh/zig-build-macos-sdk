41e1e3b67a6be4d1d621972514cf4209747ef7ad
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2023-10-07 15:22:33 -0700

    export addpaths

--- build.zig
+++ build.zig
@@ -11,8 +11,15 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
     lib.linkLibC();
-    lib.addSystemFrameworkPath(.{ .path = "Frameworks" });
-    lib.addSystemIncludePath(.{ .path = "include" });
-    lib.addLibraryPath(.{ .path = "lib" });
+    try addPaths(b, lib);
     b.installArtifact(lib);
+}
+
+pub fn addPaths(b: *std.Build, step: *std.build.CompileStep) !void {
+    // https://github.com/ziglang/zig/issues/17358
+    if (step.target.isNative()) b.sysroot = "/";
+
+    step.addSystemFrameworkPath(.{ .path = "Frameworks" });
+    step.addSystemIncludePath(.{ .path = "include" });
+    step.addLibraryPath(.{ .path = "lib" });
 }
